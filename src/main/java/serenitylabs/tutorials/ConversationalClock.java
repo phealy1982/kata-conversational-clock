7077b1973982c40acaa7b08fee413b987d25f7ab
phealy1982 <phealy1982@gmail.com>
2016-09-25 14:26:08 +0100

    attempt two

--- src/main/java/serenitylabs/tutorials/ConversationalClock.java
+++ src/main/java/serenitylabs/tutorials/ConversationalClock.java
@@ -8,33 +8,36 @@ public class ConversationalClock {
     public static final String START_OF_SENTENCE = "it's" + SPACE;
     private final SystemTime now;
     private final int nearestHour;
+    private final Boolean onTheHour;
+    private final Boolean noonOrMidnight;
 
     public ConversationalClock(SystemTime time) {
         this.now = time;
         this.nearestHour = nearestHour(this.now.minute());
+        this.onTheHour = onTheHour(this.now.minute());
+        this.noonOrMidnight = noonOrMidnight(this.now.hour(), this.now.minute());
+    }
+
+    private Boolean onTheHour(int minute) {
+        return minute == 0;
     }
 
     private int nearestHour(int minute) {
-        return minute>30 ?
-            now.hour()+ONE_HOUR:
-            now.hour();
+        return minute > 30 ? now.hour() + ONE_HOUR : now.hour();
     }
 
     public String currentTime() {
-        return createSentence(now.hour(), now.minute());
+        return createSentence(now.minute());
     }
 
-    private String createSentence(int hour, int minute) {
+    private String createSentence(int minute) {
         String sentence = START_OF_SENTENCE;
-        String hourPart;
+        String hourPart = ConversationalHour.wordFor(nearestHour);
         String minutePart = "";
 
-        if (onTheHour(minute)) {
-            hourPart = noonOrMidnight(hour, minute) ?
-                ConversationalHour.wordFor(hour):
-                ConversationalHour.wordFor(hour) + O_CLOCK;
-        } else {
-            hourPart = ConversationalHour.wordFor(nearestHour);
+        if (onTheHour && !noonOrMidnight) {
+                hourPart += O_CLOCK;
+        } else if(!onTheHour){
             minutePart = ConversationalMinute.wordFor(minute) + SPACE;
         }
         return sentence + minutePart + hourPart;
@@ -42,9 +45,5 @@ public class ConversationalClock {
 
     private boolean noonOrMidnight(int hour, int minute) {
         return (hour == 0 || hour == 12) && minute == 0;
-    }
-
-    private boolean onTheHour(int minute) {
-        return minute == 0;
     }
 }
